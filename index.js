e396b87567c8433c4d9894e7c5c9a9a25a567883
mdha <mdha@dr.dk>
2018-10-01 17:44:53 +0300

    testing dialogflow

--- index.js
+++ index.js
@@ -2,13 +2,27 @@ require('dotenv').config()
 const MovieDb = require('moviedb-promise')
 const moviedb = new MovieDb(process.env.APIKEY)
 const express = require('express')
+const bodyParser = require('body-parser')
+const {
+    dialogflow,
+    SimpleResponse,
+} = require('actions-on-google')
 
-const app = express()
+const app = dialogflow()
+const expressApp = express().use(bodyParser.json())
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello from smarter speaker!'));
+app.intent('test', conv => {
+    conv.ask(new SimpleResponse({
+        displayText: "Test response text"
+    }))
+})
 
-app.get('/search/actor', (req, res) => {
+expressApp.post('/fulfillment', app)
+
+expressApp.get('/', (req, res) => res.send('Hello from smarter speaker!'));
+/*
+expressApp.get('/search/actor', (req, res) => {
     let query = req.query.q
     moviedb.searchPerson({ query: query }).then(actorResult => {
         moviedb.discoverMovie({ 'with_people': actorResult.results[0].id }).then(movieResult => {
@@ -16,7 +30,7 @@ app.get('/search/actor', (req, res) => {
         })
     }).catch(console.error);
 
-});
+});*/
 
 app.listen(port, () => console.log(`Smarter speaker app listening on port ${port}!`))
 
