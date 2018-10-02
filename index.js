92548aaa679461570b1fdbd00c8d6638a4e99ed1
mdha <mdha@dr.dk>
2018-10-02 12:25:59 +0300

    final

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.intent('test', async conv => {
     genreData[genreTitles[1].name] = genreTitles[1].id;
     conv.data.actor = actorResult.results[0].id;
     conv.data.genreData = genreData;
-    conv.ask('testing');
+    conv.ask(`Do you want to watch a ${genreTitles[0].name} or a ${genreTitles[1].name} movie`);
 
 
     /*if(movieResult.results.length > 10){
@@ -63,12 +63,12 @@ app.intent('test - custom', async conv => {
     let movieResult = await moviedb.discoverMovie({ 'with_people': conv.data.actor, 'with_genres': genreId })
     conv.data.movieResult = movieResult;
     conv.data.count = 0;
-    conv.ask(movieResult.results[conv.data.count].title);
+    conv.ask(`Is ${movieResult.results[conv.data.count].title} the movie you are looking for`);
 })
 
 app.intent('test - custom - no', conv => {
     conv.data.count++;
-    conv.ask(conv.data.movieResult.results[conv.data.count].title);
+    conv.ask(`Is ${movieResult.results[conv.data.count].title} the movie you are looking for`);
 })
 
 expressApp.get('/healthcheck', (req, res) => res.sendStatus(200));
