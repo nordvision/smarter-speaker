0cebce9e4845b2c2c6f987953c3794350daf75bd
mdha <mdha@dr.dk>
2018-10-02 10:14:54 +0300

    added suggestions

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const MovieDb = require('moviedb-promise')
 const moviedb = new MovieDb(process.env.TMDB_APIKEY)
 const express = require('express')
 const bodyParser = require('body-parser')
+const _ = require('lodash')
 const {
     dialogflow,
     SimpleResponse,
-    TransactionDecision
+    TransactionDecision,
+    Suggestions
 } = require('actions-on-google')
 const app = dialogflow()
 const expressApp = express().use(bodyParser.json())
@@ -16,7 +18,29 @@ app.intent('test', async conv => {
     let actorResult = await moviedb.searchPerson({ query: conv.parameters.actors });
 
     let movieResult = await moviedb.discoverMovie({ 'with_people': actorResult.results[0].id })
-    if(movieResult.results.length > 10){
+    let genreList = await moviedb.genreMovieList();
+    let genreIds = [];
+
+
+    for (let index = 0; index < movieResult.results.length; index++) {
+        const resultElement = movieResult.results[index];
+        genreIds = _.union(genreIds, resultElement.genre_ids)
+    }
+    let genreTitles = [];
+    console.log(genreList);
+    _.forEach(genreIds, id => {
+        genreTitles.push(_.find(genreList.genres, genre => {
+            console.log(id);
+            return genre.id == id
+        }))
+    })
+    let genreResponse = []
+    genreResponse.push(genreTitles[0].name);
+    genreResponse.push(genreTitles[1].name);
+    conv.ask(new Suggestions(genreResponse));
+
+
+    /*if(movieResult.results.length > 10){
         conv.ask(new TransactionDecision({
             intent: 'test2'
         }))
@@ -25,9 +49,7 @@ app.intent('test', async conv => {
             speech: movieResult.results[0].title,
             text: movieResult.results[0].title
         }))
-    }
-
-
+    }*/
 })
 expressApp.get('/healthcheck', (req, res) => res.sendStatus(200));
 
